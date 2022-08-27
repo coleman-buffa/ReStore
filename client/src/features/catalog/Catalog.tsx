bd28511868a64faef11b570b4e4d6c929a5b97ea
Coleman Buffa <coleman.buffa@gmail.com>
2022-08-27 09:33:15 -0700

    Replace state management with Redux

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import ProductList from "./ProductList";
+
+export default function Catalog() {
+    const products = useAppSelector(productSelectors.selectAll);
+    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (!productsLoaded) dispatch(fetchProductsAsync());
+    }, [productsLoaded, dispatch])
+
+    if (status.includes('pending')) return <LoadingComponent message='Loading products...' />
+
+    return (
+        <>
+            <ProductList products={products} />
+        </>
+    )
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import { StoreProvider } from './app/context/StoreContext';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
 
 export const history = createBrowserHistory();
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <Router history={history}>
-    <React.StrictMode>
-      <StoreProvider>
+ReactDOM.render(
+  <React.StrictMode>
+    <Router history={history}>
+      <Provider store={store}>
         <App />
-      </StoreProvider>
-    </React.StrictMode>
-  </Router>
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
